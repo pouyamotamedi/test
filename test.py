75355cd3f6383e86815357eba6c056439a95f164
pouyamotamedi <97619333+pouyamotamedi@users.noreply.github.com>
2022-03-21 13:29:11 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,70 @@
+# v = [2, 3, 3, 3, 4, 4, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8]
+
+
+# # def show(lst, target):
+# #     begin = 0
+# #     end = len(lst)-1
+
+# #     while begin <= end:
+# #         mid = (begin + end) // 2
+
+# #         if lst[mid] > target:
+# #             end = mid - 1
+# #         else:
+# #             begin = mid
+
+# #         if lst[end] == target:
+# #             return end
+
+
+
+# # print(show(v, 8),  list(enumerate(v)), end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class person():
+#     def __init__(self, name):
+#         self._name = name
+
+
+#     # @property
+#     # def nm(self):
+#     #     return self._name
+
+#     @self._name.setter
+#     def nm(self, val):
+#         if not isinstance(val, str):
+#             raise ValueError('he he he')
+#         else:
+#             self._name = val
+
+
+
+
+
+# a = person('ali')
+# a.nm = 23#"telli"
+# print(a.nm)
+
+
+v = [1, 2, 3, 4, 5, 6]
+
+
+
+x = "hi"
+assert x == "h0i"
